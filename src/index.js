f311c6b9a5bbccba848f7eadff0bd33e05a7dc3a
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-12-25 05:28:51 +0600

    done adding

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const dotenv = require('dotenv').config();
 const app = express();
-const port =  process.env.PORT  ;
+const port =  process.env.PORT || 5000  ;
 const colors = require('colors');
 const cors = require('cors');
 const dbconnect = require('./dbconnect');
